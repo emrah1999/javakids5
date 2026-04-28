e4956de676a63e8e41555b8366ffd15e423d32e7
emrah1999 <emraheyubovemrah@gmail.com>
2026-04-28 21:24:14 +0400

    Dərs 6. Java main metodu, Expression,Statetment

---
+++ src/lesson6ProgramWOrking/Main2.java
@@ -0,0 +1,24 @@
+package lesson6ProgramWOrking;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int a=6;
+
+        System.out.println(a==5?"Hello World":"Hello Java");
+
+        String s="Salam";
+
+        String s1="Ibrahim";
+        System.out.println(s+" "+s1);
+        if(a==5){
+            int b=6;
+            System.out.println(b);
+        }
+
+
+    }
+
+
+
+
+}
---
+++ src/lesson6ProgramWOrking/World.java
@@ -0,0 +1,32 @@
+package lesson6ProgramWOrking;
+
+public class World {
+
+    public static void main(String[] args) {
+        if(args.length>0){
+            System.out.println("Hello " + args[0]);
+        } else{
+            System.out.println("Hello World");
+        }
+        int a=5;
+
+        System.out.println(++a);
+//        System.out.println(a);
+    }
+
+    public static void main(int a) {
+        System.out.println("Hello World");
+    }
+
+    void a(){
+
+    }
+
+    void a(int a){
+
+    }
+    void a(int a,int v){
+
+    }
+}
+
