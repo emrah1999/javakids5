fb3254943c28f63200ca8f3f94a2f0741b89827e
emrah1999 <emraheyubovemrah@gmail.com>
2026-05-09 17:08:13 +0400

    Dərs 9. Dəyişənlərin yadda saxlanması və indentifaktorlar

--- src/son8LiteralTipCevirlmesi/Main3.java
+++ src/son8LiteralTipCevirlmesi/Main3.java
@@ -7,5 +7,23 @@ public class Main3 {
         System.out.println("Short a: "+a);
         byte b=(byte) a;
         System.out.println("Byte b: "+b);
+
+        short c=500;
+        byte d=(byte) c;
+        System.out.println(d);
+
+        int m=231832341;
+        short n=(short) m;
+        System.out.println(n);
+
+        int aa=Integer.MAX_VALUE;
+
+        int integer=23;
+
+        int d4=4;
+        int _f=3;
+        int $r=3;
+    }
+    static void main2(){
     }
 }
