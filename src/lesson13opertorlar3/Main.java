5be2cd9e5bacf06856ef9ce349d8356b5c31bcfb
emrah1999 <emraheyubovemrah@gmail.com>
2026-05-23 17:17:14 +0400

    Dərs 13. Operatorlar 3ci hissə, Bitwise, Ternary opertorları, Operatorların üstünlük sırası

---
+++ src/lesson13opertorlar3/Main.java
@@ -0,0 +1,13 @@
+package lesson13opertorlar3;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=-10;
+
+        System.out.println(a>>1);
+
+        System.out.println(a>0?"pozitif":"negatif");
+
+        int b=a>0?3:0;
+    }
+}
